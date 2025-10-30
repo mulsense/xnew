71e9e8788a084f42aa0159580712047470a209a0
wisroot-com <hisayoshi.furihata@wisroot.com>
2025-10-31 02:02:18 +0900

    mod tests

--- test/unit.relation.test.ts
+++ test/unit.relation.test.ts
@@ -9,23 +9,23 @@ describe('unit relation', () => {
     it('basic', () => {
         const unit1 = xnew();
         const unit2 = xnew(unit1);
-        expect(unit1._.inputs.parent).toBe(null);
-        expect(unit2._.inputs.parent).toBe(unit1);
+        expect(unit1._.parent).toBe(null);
+        expect(unit2._.parent).toBe(unit1);
     });
 
     it('nest', () => {
         xnew((self: xnew.Unit) => {
             const unit2 = xnew();
-            expect(self._.inputs.parent).toBe(null);
-            expect(unit2._.inputs.parent).toBe(self);
+            expect(self._.parent).toBe(null);
+            expect(unit2._.parent).toBe(self);
         })
     });
 
     it('delete', () => {
         xnew((self: xnew.Unit) => {
             const unit2 = xnew();
-            expect(self._.inputs.parent).toBe(null);
-            expect(unit2._.inputs.parent).toBe(self);
+            expect(self._.parent).toBe(null);
+            expect(unit2._.parent).toBe(self);
         })
     });
 });
