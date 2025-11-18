811e94a5290fd7df09e199ffe0cf9cd3b5fe1635
mulsense <mulsense.root@gmail.com>
2025-11-18 23:39:03 +0900

    mod test

--- test/time.test.ts
+++ test/time.test.ts
@@ -6,12 +6,12 @@ describe('timer', () => {
             let count = 0;
             let start = Date.now();
             const margin = 100;
-            const timer = new Timer(null, () => {
+            const timer = new Timer({ timeout: () => {
                 count++;
                 const d = Date.now() - start;
                 expect(d).toBeGreaterThan(500 - margin);
                 expect(d).toBeLessThan(500 + margin);
-            }, 500);
+            }, duration: 500 });
             setTimeout(() => count === 1 ? resolve() : reject(), 500 + margin);
         });
     });
@@ -21,12 +21,12 @@ describe('timer', () => {
             let count = 0;
             let start = Date.now();
             const margin = 100;
-            const timer = new Timer(null, () => {
+            const timer = new Timer({ timeout: () => {
                 count++;
                 const d = Date.now() - start;
                 expect(d).toBeGreaterThan(500 * count - margin);
                 expect(d).toBeLessThan(500 * count + margin);
-            }, 500, { loop: true });
+            }, duration: 500, loop: true });
             setTimeout(() => count === 2 ? resolve() : reject(), 1000 + margin);
         });
     });
