59b1cb721c1e7639f24b76a02caf88897b2a75b5
mulsense <mulsense.root@gmail.com>
2025-12-22 21:56:18 +0900

    update

--- examples/3_three/103_model/script.js
+++ examples/3_three/103_model/script.js
@@ -123,7 +123,7 @@ function Model(unit, { gltf }) {
   }
 
   const clock = new THREE.Clock();
-  unit.on('-update', () => {
+  unit.on('update', () => {
     mixer.update(clock.getDelta());
   });
 }
