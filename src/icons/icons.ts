ef110ee5d604697108ddf54f0e223766d74c89d4
mulsense <mulsense.root@gmail.com>
2026-01-10 01:33:16 +0900

    refactoring

--- src/icons/icons.ts
+++ src/icons/icons.ts
@@ -14,34 +14,36 @@ function OutLineTemplate(unit: Unit,
     >`);
 }
 
+function createIcon(props: Object, paths: string[]) {
+    xnew.nest(`<div style="position: relative; width: 100%; height: 100%;">`);
+    xnew((unit: Unit) => {
+        xnew.extend(OutLineTemplate, props);
+        for (const path of paths) {
+            xnew(path);
+        }
+    });
+}
+
 export const icons = {
   AcademicCap(unit: Unit, props: Object) {
-    xnew.nest(`<div style="position: relative; width: 100%; height: 100%;">`);
-    xnew((unit: Unit) => {
-      xnew.extend(OutLineTemplate, props);
-      xnew('<path d="M4.26 10.147a60 60 0 0 0-.491 6.347A48.6 48.6 0 0 1 12 20.904a48.6 48.6 0 0 1 8.232-4.41a61 61 0 0 0-.491-6.347m-15.482 0a51 51 0 0 0-2.658-.813A60 60 0 0 1 12 3.493a60 60 0 0 1 10.399 5.84q-1.345.372-2.658.814m-15.482 0A51 51 0 0 1 12 13.489a50.7 50.7 0 0 1 7.74-3.342M6.75 15a.75.75 0 1 0 0-1.5a.75.75 0 0 0 0 1.5m0 0v-3.675A55 55 0 0 1 12 8.443m-7.007 11.55A5.98 5.98 0 0 0 6.75 15.75v-1.5" />')
-    });
+    createIcon(props, [
+        '<path d="M4.26 10.147a60 60 0 0 0-.491 6.347A48.6 48.6 0 0 1 12 20.904a48.6 48.6 0 0 1 8.232-4.41a61 61 0 0 0-.491-6.347m-15.482 0a51 51 0 0 0-2.658-.813A60 60 0 0 1 12 3.493a60 60 0 0 1 10.399 5.84q-1.345.372-2.658.814m-15.482 0A51 51 0 0 1 12 13.489a50.7 50.7 0 0 1 7.74-3.342M6.75 15a.75.75 0 1 0 0-1.5a.75.75 0 0 0 0 1.5m0 0v-3.675A55 55 0 0 1 12 8.443m-7.007 11.55A5.98 5.98 0 0 0 6.75 15.75v-1.5" />'
+    ]);
   },
   AdjustmentsHorizontal(unit: Unit, props: Object) {
-    xnew.nest(`<div style="position: relative; width: 100%; height: 100%;">`);
-    xnew((unit: Unit) => {
-      xnew.extend(OutLineTemplate, props);
-      xnew('<path d="M10.5 6h9.75M10.5 6a1.5 1.5 0 1 1-3 0m3 0a1.5 1.5 0 1 0-3 0M3.75 6H7.5m3 12h9.75m-9.75 0a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m-3.75 0H7.5m9-6h3.75m-3.75 0a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m-9.75 0h9.75" />')
-    });
+    createIcon(props, [
+        '<path d="M10.5 6h9.75M10.5 6a1.5 1.5 0 1 1-3 0m3 0a1.5 1.5 0 1 0-3 0M3.75 6H7.5m3 12h9.75m-9.75 0a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m-3.75 0H7.5m9-6h3.75m-3.75 0a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m-9.75 0h9.75" />'
+    ]);
   },
   AdjustmentsVertical(unit: Unit, props: Object) {
-    xnew.nest(`<div style="position: relative; width: 100%; height: 100%;">`);
-    xnew((unit: Unit) => {
-      xnew.extend(OutLineTemplate, props);
-      xnew('<path d="M6 13.5V3.75m0 9.75a1.5 1.5 0 0 1 0 3m0-3a1.5 1.5 0 0 0 0 3m0 3.75V16.5m12-3V3.75m0 9.75a1.5 1.5 0 0 1 0 3m0-3a1.5 1.5 0 0 0 0 3m0 3.75V16.5m-6-9V3.75m0 3.75a1.5 1.5 0 0 1 0 3m0-3a1.5 1.5 0 0 0 0 3m0 9.75V10.5" />')
-    });
+    createIcon(props, [
+        '<path d="M6 13.5V3.75m0 9.75a1.5 1.5 0 0 1 0 3m0-3a1.5 1.5 0 0 0 0 3m0 3.75V16.5m12-3V3.75m0 9.75a1.5 1.5 0 0 1 0 3m0-3a1.5 1.5 0 0 0 0 3m0 3.75V16.5m-6-9V3.75m0 3.75a1.5 1.5 0 0 1 0 3m0-3a1.5 1.5 0 0 0 0 3m0 9.75V10.5" />'
+    ]);
   },
   ArchiveBox(unit: Unit, props: Object) {
-    xnew.nest(`<div style="position: relative; width: 100%; height: 100%;">`);
-    xnew((unit: Unit) => {
-      xnew.extend(OutLineTemplate, props);
-      xnew('<path d="m20.25 7.5l-.625 10.632a2.25 2.25 0 0 1-2.247 2.118H6.622a2.25 2.25 0 0 1-2.247-2.118L3.75 7.5M10 11.25h4M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125" />')
-    });
+    createIcon(props, [
+        '<path d="m20.25 7.5l-.625 10.632a2.25 2.25 0 0 1-2.247 2.118H6.622a2.25 2.25 0 0 1-2.247-2.118L3.75 7.5M10 11.25h4M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125" />'
+    ]);
   },
   ArchiveBoxArrowDown(unit: Unit, props: Object) {
     xnew.nest(`<div style="position: relative; width: 100%; height: 100%;">`);
