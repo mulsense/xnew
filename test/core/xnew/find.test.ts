c6ac93e8fa8a46d64d3289357f349354fbd98fed
mulsense <mulsense.root@gmail.com>
2026-06-03 04:43:38 +0900

    test(core): add xnew/find.test.ts

---
+++ test/core/xnew/find.test.ts
@@ -0,0 +1,30 @@
+import { Unit } from '../../../src/core/unit';
+import { xnew } from '../../../src/core/xnew';
+
+describe('xnew.find', () => {
+    beforeEach(() => { Unit.reset(); });
+    afterEach(() => { Unit.rootUnit?.finalize(); });
+
+    it('returns every unit registered under the given component', () => {
+        function A(_: Unit) {}
+        function B(_: Unit) {}
+        let a1!: Unit, a2!: Unit, b1!: Unit;
+        xnew(() => { a1 = xnew(A); a2 = xnew(A); b1 = xnew(B); });
+        expect(xnew.find(A)).toEqual(expect.arrayContaining([a1, a2]));
+        expect(xnew.find(A)).toHaveLength(2);
+        expect(xnew.find(B)).toEqual([b1]);
+    });
+
+    it('returns an empty array when no unit matches', () => {
+        function Absent(_: Unit) {}
+        expect(xnew.find(Absent)).toEqual([]);
+    });
+
+    it('drops a unit from the index after finalize', () => {
+        function A(_: Unit) {}
+        const unit = xnew(A);
+        expect(xnew.find(A)).toContain(unit);
+        unit.finalize();
+        expect(xnew.find(A)).not.toContain(unit);
+    });
+});
